2054af1384fcd098d18aa423b4cf470531122fd3
Shriya Mahakala <shriya.mahakala@gmail.com>
2020-01-06 17:05:51 -0500

    created a new subsystem

---
+++ src/main/java/frc/robot/subsystems/Subsystem.java
@@ -0,0 +1,14 @@
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Subsystem extends Subsystem {
+    public Subsystem() {
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       e.g. setDefaultCommand(new MyCommand());
+    }
+}
+
