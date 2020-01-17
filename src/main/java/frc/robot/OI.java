2230fd85dbc4836c3d0a438057a200c01315c1d5
Rowan-Ponelis <55511967+Rowan-Ponelis@users.noreply.github.com>
2020-01-16 22:12:50 -0500

    No errors

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,9 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
 public class OI {
 
-	public OI() {
-
-	}
+	private Joystick left, right;
+	private XboxController xbox;
+	private static final double percentDeadbandThreshold = 0.1;
 
 }
