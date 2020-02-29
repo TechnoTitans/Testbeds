01ac36abeab14156d8c33c6dd77788b208d7fb0e
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2020-02-29 15:26:44 -0500

    keep workin on vision

--- src/main/java/frc/robot/sensors/Vision.java
+++ src/main/java/frc/robot/sensors/Vision.java
@@ -1,6 +1,7 @@
 package frc.robot.sensors;
 
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -9,20 +10,30 @@ public class Vision {
 
     SerialPort visionData;
 
+    /**
+     * The number of values (doubles) we'll be recieving from vision
+     */
+    private static final int NUM_VALUES = 5;
+    private static final int BYTES_FROM_DEVICE = NUM_VALUES * 8;
+
     public Vision() {
-        visionData = new SerialPort(9600, SerialPort.Port.kUSB);
+        visionData = new SerialPort(115200, SerialPort.Port.kUSB);
     }
 
+    // TODO make this work
     public double[] getData() {
         double[] data = new double[5];
         int c = 0;
 
-        byte[] visionDataBytes = visionData.read(40); //if no target is detected returns all -1
+        byte[] visionDataBytes = visionData.read(BYTES_FROM_DEVICE); //if no target is detected returns all -1
 
         for (int i = 0; i < 40; i += 8) {
-            data[c] = ByteBuffer.wrap(Arrays.copyOfRange(visionDataBytes, i, i + 7)).getDouble();
+            data[c] = ByteBuffer.wrap(Arrays.copyOfRange(visionDataBytes, i, i + 8)).getDouble();
             c++;
         }
+        SmartDashboard.putString("Byte Array of Vision Data", Arrays.toString(visionDataBytes));
+        SmartDashboard.putNumberArray("Double Array of Vision Data", (data));
+
         return data;
     }
 
@@ -40,7 +51,7 @@ public class Vision {
      *
      * @return distance in inches
      */
-    public double getDistance() {
+    public double getVisionDistance() {
         return getData()[2];
     }
 
@@ -66,14 +77,31 @@ public class Vision {
         return A + B * distance + C * Math.pow(distance, 2) + D * Math.pow(distance, 3);
     }
 
-    // TODO MAKE SURE
-    public double getHoodAngle() {
-        return -9999;
+    /**
+     *
+     * @param distance distance away from target in inches
+     * @return optimal encoder position for hood
+     */
+    public double getHoodAngle(double distance) {
+        double A = 3482,
+                B = -63.07,
+                C = 0.3093,
+                D = -0.00005104;
+
+        return A + B * distance + C * Math.pow(distance, 2) + D * Math.pow(distance, 3);
     }
 
 
     public double getCurrentOptimalRPM() {
-        return getRPMFromDistance(this.getDistance());
+        return getRPMFromDistance(this.getVisionDistance());
+    }
+
+    /**
+     *
+     * @return - optimal hood position in encoder ticks
+     */
+    public double getCurrentOptimalHoodAngle() {
+        return getHoodAngle(this.getVisionDistance());
     }
 
 
