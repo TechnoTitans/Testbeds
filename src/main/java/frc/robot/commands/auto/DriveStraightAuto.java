4cc6364499c938fa0f31123d28bd7d43c79e6f86
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2020-02-29 15:33:03 -0500

    fix DriveStraight autonomous

--- src/main/java/frc/robot/commands/auto/DriveStraightAuto.java
+++ src/main/java/frc/robot/commands/auto/DriveStraightAuto.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpiutil.math.MathUtil;
+import frc.robot.motor.Filter;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.TankDrive;
 
@@ -15,6 +17,7 @@ public class DriveStraightAuto extends CommandBase {
 	private final TankDrive driveTrain;
 	private final double desiredDistance;
 	private final double desiredSpeed;
+	private final Filter speedFilter;
 
 	/**
 	 *
@@ -25,20 +28,31 @@ public class DriveStraightAuto extends CommandBase {
 	public DriveStraightAuto(TankDrive driveTrain, double distance, double speed) {
 		super();
 		this.driveTrain = driveTrain;
+//		this.desiredDistance = distance;
 		this.desiredDistance = MathUtil.clamp(distance, -MAX_DISTANCE, MAX_DISTANCE);
 		// if we want to go backwards, the speed must be negative
-		this.desiredSpeed = Math.copySign(desiredDistance, speed);
+//		this.desiredSpeed = speed;
+		this.desiredSpeed = Math.copySign(speed, desiredDistance);
+		this.speedFilter = new Filter(0.5);
+		this.speedFilter.update(0); // start the filter off at zero
 		addRequirements(driveTrain);
+		SmartDashboard.putNumber("desired distance", desiredDistance);
+		SmartDashboard.putNumber("desired speed", desiredSpeed);
 	}
 
 	@Override
 	public void initialize() {
 		this.driveTrain.resetEncoders();
+		this.speedFilter.setValue(0); // reset the filter so that subsequent autonomouses work
 	}
 
 	@Override
 	public void execute() {
-		this.driveTrain.set(this.desiredSpeed);
+		speedFilter.update(this.desiredSpeed);
+		// TODO Filter setpoint at the TitanFX class level so that we don't have to keep filtering stuff
+		this.driveTrain.set(speedFilter.getValue());
+		SmartDashboard.putNumber("Right encoder value", driveTrain.getRightEncoder().getDistance());
+		SmartDashboard.putNumber("DriveTrain percentoutput", driveTrain.getRight().get());
 	}
 
 	@Override
@@ -48,6 +62,12 @@ public class DriveStraightAuto extends CommandBase {
 
 	@Override
 	public boolean isFinished() {
-		return driveTrain.getRightEncoder().getDistance() >= this.desiredDistance;
+		// TODO Enforce max distance here with condition
+		if (Math.signum(this.desiredDistance) == -1) {
+			return driveTrain.getRightEncoder().getDistance() <= this.desiredDistance;
+
+		} else {
+			return driveTrain.getRightEncoder().getDistance() >= this.desiredDistance;
+		}
 	}
 }
