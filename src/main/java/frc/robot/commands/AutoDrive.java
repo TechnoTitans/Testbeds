19dd3ed500dd8eabb6e4a787afda46aba3d1d92d
Cyrus <cyrusyong1@gmail.com>
2021-01-27 16:43:56 -0500

    test

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -2,35 +2,48 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.TankDrive;
+import edu.wpi.first.wpilibj.Timer;
 
-public class ExampleCommand extends CommandBase {
+public class AutoDrive extends CommandBase {
 
     private final TankDrive driveTrain;
-    private final int MAX_DISTANCE = 60;
+    private final Timer timer;
 
-    public ExampleCommand(TankDrive driveTrain) {
+
+    public AutoDrive(TankDrive driveTrain, Timer timer) {
         this.driveTrain = driveTrain;
+        this.timer = timer;
         addRequirements(driveTrain);
     }
 
     @Override
     public void initialize() {
+        //Turn on motor
         this.driveTrain.getGyro().reset();
+        this.driveTrain.getGyro().calibrate();
+
+        //Start Timer
+        timer.start();
     }
 
     @Override
     public void execute() {
-        this.driveTrain.set(0.5);
+        //Drive while timer is less than 5 sec
+        do{
+            this.driveTrain.set(0.5);
+        }
+        while(!timer.hasElapsed(5));
+
     }
 
     @Override
     public void end(boolean interrupted) {
-        this.driveTrain.brake();
+        this.driveTrain.stop();
+        timer.reset();
     }
 
     @Override
     public boolean isFinished() {
-//        return this.driveTrain.getLeftEncoder().getDistance() > MAX_DISTANCE;
         return true;
     }
 }
